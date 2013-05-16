723f1256883bcbdbd54fe3587d78d3fdbe9c7b09
Romain Prieto <romain.prieto@gmail.com>
2013-05-16 19:45:23 +1000

    Controllers navigate from 1 page to the other

--- public/device/device.js
+++ public/device/device.js
@@ -5,7 +5,7 @@ window.Pong.Device = {};
 Pong.Device = function() {
   
   routie({
-      '/':         Pong.Device.Register,
+      '':          Pong.Device.Register,
       '/register': Pong.Device.Register,
       '/wait':     Pong.Device.Wait,
       '/join':     Pong.Device.Join,
@@ -19,28 +19,43 @@ Pong.Device.Register = function() {
   var source   = $("#tmpl-register").html();
   var template = Handlebars.compile(source);
   $('#page').html(template());
+  $('#register').on('click', function() {
+    routie.navigate('/wait');
+  });
 };
 
 Pong.Device.Wait = function() {
   var source   = $("#tmpl-wait").html();
   var template = Handlebars.compile(source);
   $('#page').html(template());
+  setTimeout(function() {
+    routie.navigate('/join');
+  }, 2000);
 };
 
 Pong.Device.Join = function() {
   var source   = $("#tmpl-join").html();
   var template = Handlebars.compile(source);
   $('#page').html(template());
+  $('#join').on('click', function() {
+    routie.navigate('/gamepad');
+  });
 };
 
 Pong.Device.Gamepad = function() {
   var source   = $("#tmpl-gamepad").html();
   var template = Handlebars.compile(source);
   $('#page').html(template());
+  $('#exit').on('click', function() {
+    routie.navigate('/thanks');
+  });
 };
 
 Pong.Device.Thanks = function() {
   var source   = $("#tmpl-thanks").html();
   var template = Handlebars.compile(source);
   $('#page').html(template());
+  setTimeout(function() {
+    routie.navigate('/');
+  }, 2000);
 };
