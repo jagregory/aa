1174de3f598d3528d12b54f35990eb6c0ab04d33
Romain Prieto <romain.prieto@gmail.com>
2013-05-16 19:38:35 +1000

    Split device into 5 controllers using hash routing

--- public/device/device.js
+++ public/device/device.js
@@ -1,11 +1,46 @@
 
 window.Pong = {}
+window.Pong.Device = {};
 
-window.Pong.Mobile = function() {
+Pong.Device = function() {
   
+  routie({
+      '/':         Pong.Device.Register,
+      '/register': Pong.Device.Register,
+      '/wait':     Pong.Device.Wait,
+      '/join':     Pong.Device.Join,
+      '/gamepad':  Pong.Device.Gamepad,
+      '/thanks':   Pong.Device.Thanks,
+  });
+  
+};
+
+Pong.Device.Register = function() {
   var source   = $("#tmpl-register").html();
   var template = Handlebars.compile(source);
-  
   $('#page').html(template());
-  
+};
+
+Pong.Device.Wait = function() {
+  var source   = $("#tmpl-wait").html();
+  var template = Handlebars.compile(source);
+  $('#page').html(template());
+};
+
+Pong.Device.Join = function() {
+  var source   = $("#tmpl-join").html();
+  var template = Handlebars.compile(source);
+  $('#page').html(template());
+};
+
+Pong.Device.Gamepad = function() {
+  var source   = $("#tmpl-gamepad").html();
+  var template = Handlebars.compile(source);
+  $('#page').html(template());
+};
+
+Pong.Device.Thanks = function() {
+  var source   = $("#tmpl-thanks").html();
+  var template = Handlebars.compile(source);
+  $('#page').html(template());
 };
