c579559254ea96b58ebf8528011e3a3c91db975b
Romain Prieto <romain.prieto@gmail.com>
2013-05-17 18:03:18 +1000

    forever "monitor" also restarts the app when the server code changes

--- monitor.js
+++ monitor.js
@@ -4,10 +4,17 @@ var options = {
   command: 'node',
   silent: false,
   minUptime: 5000,
-  spinSleepTime: 500
+  spinSleepTime: 500,
+  watch: true,
+  watchDirectory: './src',
+  watchIgnoreDotFiles: true
 };
 
 var monitor = new forever.Monitor('server.js', options);
+
+monitor.on('watch:restart', function(info) {
+    return console.error("restarting script because " + info.file + " changed");
+});
 
 monitor.on('restart', function() {
     return console.error("Forever restarting script for " + monitor.times + " time");
