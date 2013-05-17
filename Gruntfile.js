b4d5013bb51adb4490ec2eb72f3fa7b5b95aae03
Romain Prieto <romain.prieto@gmail.com>
2013-05-17 12:37:31 +1000

    Cleaned up Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,52 +1,68 @@
-module.exports = function(grunt) {
+var config = {};
 
-  function processHandlebars(bundle) {
-    bundle.transform(require('hbsfy'));
-  }
-
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    stylus: {
-      options: {
-        compress: true
-      },
-      compile: {
-        files: {
-          'builtAssets/device/device.css': 'assets/device/device.styl'
-        }
-      }
-    },
-    browserify2: {
-      dev: {
-        entry: __dirname + '/assets/device/device.js',
-        beforeHook: processHandlebars,
-        compile: __dirname + '/builtAssets/device/device.js',
-        debug: true
-      }
-    },
-    copy: {
-      main: {
-        files: [
-          { expand: true, cwd: 'assets/device/', src: ['index.html'], dest: 'builtAssets/device/' },
-          { expand: true, cwd: 'files/', src: ['**'], dest: 'builtAssets/'}
-        ]
-      }
-    },
-    watch: {
-      js: {
-        files: ['assets/**/*.js', 'assets/**/*.hbs'],
-        tasks: ['browserify2:dev'],
-      },
-      css: {
-        files: ['assets/**/*.styl'],
-        tasks: ['stylus'],
-      },
-      files: {
-        files: ['files/**/*', 'assets/**/*.html'],
-        tasks: ['copy'],
-      }
+//
+// CSS
+//
+config.stylus = {
+  options: {
+    compress: true
+  },
+  compile: {
+    files: {
+      'builtAssets/device/device.css': 'assets/device/device.styl'
     }
-  });
+  }
+};
+
+//
+// JS
+//
+config.browserify2 = {
+  dev: {
+    entry: __dirname + '/assets/device/device.js',
+    beforeHook: function(bundle) {
+      bundle.transform(require('hbsfy'));
+    },
+    compile: __dirname + '/builtAssets/device/device.js',
+    debug: true
+  }
+};
+
+//
+// Static files
+//
+config.copy = {
+  main: {
+    files: [
+      { expand: true, cwd: 'assets/device/', src: ['index.html'], dest: 'builtAssets/device/' },
+      { expand: true, cwd: 'files/', src: ['**'], dest: 'builtAssets/'}
+    ]
+  }
+};
+
+//
+// Reload when files change
+//
+config.watch = {
+  js: {
+    files: ['assets/**/*.js', 'assets/**/*.hbs'],
+    tasks: ['browserify2:dev']
+  },
+  css: {
+    files: ['assets/**/*.styl'],
+    tasks: ['stylus']
+  },
+  files: {
+    files: ['files/**/*', 'assets/**/*.html'],
+    tasks: ['copy']
+  }
+};
+
+
+module.exports = function(grunt) {
+  
+  config.pkg = grunt.file.readJSON('package.json');
+  grunt.initConfig(config);
 
   require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
   grunt.registerTask('default', ['stylus', 'browserify2:dev', 'copy']);
