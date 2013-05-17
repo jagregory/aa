f51e60db4a2d1305edd0988f12c83a65a3c4cb69
James Gregory <james@jagregory.com>
2013-05-17 19:29:48 +1000

    Start to incorporate physics into our objects

--- public/game/game.js
+++ public/game/game.js
@@ -11,7 +11,15 @@ window.requestAnimFrame = (function(){
 ;(function() {
   var interval = 1000.0 / 60
 
-  var Player = function(stage, left) {
+  var box2world = function(val) {
+    return val * 50.0
+  }
+
+  var world2box = function(val) {
+    return val / 29.5
+  }
+
+  var Player = function(stage, physicsWorld) {
     var texture = PIXI.Texture.fromImage('/game/paddle.png')
     var sprite = new PIXI.Sprite(texture)
     stage.addChild(sprite)
@@ -20,8 +28,24 @@ window.requestAnimFrame = (function(){
     var boardHeight = $('#board').height()
     var boardWidth = $('#board').width()
 
-    sprite.position.x = 10
+    sprite.position.x = 30
     sprite.position.y = (boardHeight / 2) - (sprite.height / 2)
+
+    var fixDef = new Box2D.Dynamics.b2FixtureDef
+    fixDef.density = 1.0
+    fixDef.friction = 0.5
+    fixDef.restitution = 0.2
+
+    var bodyDef = new Box2D.Dynamics.b2BodyDef
+    
+    bodyDef.type = Box2D.Dynamics.b2Body.b2_staticBody
+    bodyDef.position.x = world2box(sprite.position.x)
+    bodyDef.position.y = world2box(sprite.position.y)
+    fixDef.shape = new Box2D.Collision.Shapes.b2PolygonShape
+    fixDef.shape.SetAsBox(world2box(sprite.width), world2box(sprite.height))
+    physicsWorld
+      .CreateBody(bodyDef)
+      .CreateFixture(fixDef)
 
     var target = {
       x: 0,
@@ -99,15 +123,93 @@ window.requestAnimFrame = (function(){
     }
   }
 
+  var Wall = function(stage, physicsWorld, x, y, width, height) {
+    var fixDef = new Box2D.Dynamics.b2FixtureDef
+    fixDef.density = 1.0
+    fixDef.friction = 0.5
+    fixDef.restitution = 0.2
+
+    var bodyDef = new Box2D.Dynamics.b2BodyDef
+    
+    bodyDef.type = Box2D.Dynamics.b2Body.b2_staticBody
+    bodyDef.position.x = world2box(x)
+    bodyDef.position.y = world2box(y)
+    fixDef.shape = new Box2D.Collision.Shapes.b2PolygonShape
+    fixDef.shape.SetAsBox(world2box(width), world2box(height))
+    physicsWorld
+      .CreateBody(bodyDef)
+      .CreateFixture(fixDef)
+
+    var texture = PIXI.Texture.fromImage('/game/wall.png')
+    var sprite = new PIXI.TilingSprite(texture)
+    sprite.height = height
+    sprite.width = width
+    sprite.position.x = x
+    sprite.position.y = y
+
+    stage.addChild(sprite)
+  }
+
   var Game = function(stage) {
     var time = new Time()
     var socket = io.connect('http://localhost:8080')
+
+    var physicsWorld = new Box2D.Dynamics.b2World(
+      new Box2D.Common.Math.b2Vec2(0, 10),
+      true
+    )
+
+    var topWall = new Wall(stage, physicsWorld, 0, 0, 600, 16)
+    var bottomWall = new Wall(stage, physicsWorld, 0, 384, 600, 16)
+    var leftWall = new Wall(stage, physicsWorld, 0, 0, 16, 400)
+    var rightWall = new Wall(stage, physicsWorld, 584, 0, 16, 400)
+
+    // var fixDef = new Box2D.Dynamics.b2FixtureDef
+    // fixDef.density = 1.0
+    // fixDef.friction = 0.5
+    // fixDef.restitution = 0.2
+
+    // var bodyDef = new Box2D.Dynamics.b2BodyDef
+    // bodyDef.type = Box2D.Dynamics.b2Body.b2_dynamicBody;
+    //      for(var i = 0; i < 30; ++i) {
+    //         if(Math.random() > 0.5) {
+    //            fixDef.shape = new Box2D.Collision.Shapes.b2PolygonShape;
+    //            fixDef.shape.SetAsBox(
+    //                  Math.random() + 0.1 //half width
+    //               ,  Math.random() + 0.1 //half height
+    //            );
+    //         } else {
+    //            fixDef.shape = new Box2D.Collision.Shapes.b2CircleShape(
+    //               Math.random() + 0.1 //radius
+    //            );
+    //         }
+    //         bodyDef.position.x = Math.random() * 10;
+    //         bodyDef.position.y = Math.random() * 10;
+    //         physicsWorld.CreateBody(bodyDef).CreateFixture(fixDef);
+    //      }
+
+
+    var debugDraw = new Box2D.Dynamics.b2DebugDraw()
+    debugDraw.SetSprite($('#board canvas')[0].getContext("2d"))
+    debugDraw.SetDrawScale(30.0)
+    debugDraw.SetFillAlpha(0.3)
+    debugDraw.SetLineThickness(1.0)
+    debugDraw.SetFlags(Box2D.Dynamics.b2DebugDraw.e_shapeBit | Box2D.Dynamics.b2DebugDraw.e_jointBit)
+    physicsWorld.SetDebugDraw(debugDraw)
 
     var ball = new Ball(stage)
     var players = []
 
     var tick = function() {
       time.update()
+      physicsWorld.Step(
+        1 / 60, // frame-rate
+        10,     // velocity iterations
+        10      // position iterations
+      )
+      physicsWorld.DrawDebugData()
+      physicsWorld.ClearForces()
+
       ball.tick(time.delta)
 
       players.forEach(function(player) {
@@ -116,7 +218,7 @@ window.requestAnimFrame = (function(){
     }
 
     var playerJoin = function(data) {
-      var player = new Player(stage)
+      var player = new Player(stage, physicsWorld)
       player.id = data.id
       player.name = data.name
       players.push(player)
@@ -163,7 +265,7 @@ window.requestAnimFrame = (function(){
     var board = $('#board')
 
     var stage = new PIXI.Stage(0x000000, false)
-    var renderer = PIXI.autoDetectRenderer(500, 300)
+    var renderer = new PIXI.CanvasRenderer(600, 400)
 
     board[0].appendChild(renderer.view)
 
@@ -175,7 +277,7 @@ window.requestAnimFrame = (function(){
       requestAnimFrame(function animate(delta) {
         instance.tick(delta)
 
-        renderer.render(stage)
+        // renderer.render(stage)
 
         requestAnimFrame(animate)
       })
