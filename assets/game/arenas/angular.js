228705a88df78f22c7ef106353b1026bf3ff21bd
Romain Prieto <romain.prieto@gmail.com>
2013-05-20 23:39:52 +1000

    interesting arena shapes

--- assets/game/arenas/angular.js
+++ assets/game/arenas/angular.js
@@ -1,4 +1,5 @@
 var w = require('../physics/world');
+var PI = 3.14;
 
 exports.name = 'Angular';
 
@@ -15,7 +16,7 @@ exports.walls = [
   { x:  w.width / 2, y:  w.bottom - 1, width:  w.width / 4, height: 1 },
   
   // inner diamonds
-  { x:  w.width / 3,     y:  w.height / 3,     width:  1, height: 1 },
-  { x:  w.width /3 * 2,  y:  w.height / 3 * 2, width:  1, height: 1 },
+  { x:  w.width / 3,     y:  w.height / 3,     width:  2, height: 2, rotation: PI / 4 },
+  { x:  w.width /3 * 2,  y:  w.height / 3 * 2, width:  2, height: 2, rotation: PI / 4 },
   
 ];
