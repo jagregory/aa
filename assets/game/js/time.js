f4b8058b7048be5081eb3ae02aee5286f30bca83
Romain Prieto <romain.prieto@gmail.com>
2013-05-23 01:53:07 +1000

    refactored time to look like all other modules

--- assets/game/js/time.js
+++ assets/game/js/time.js
@@ -1,28 +1,27 @@
-module.exports = (function() {
-  var time = function() {
-    this.delta = 1
-    this.lastTime = new Date()
-    this.frames = 0
+
+function Time() {
+  this.delta = 1
+  this.lastTime = new Date()
+  this.frames = 0  
+}
+
+Time.prototype.update = function() {
+  this.frames++
+  var time = Date.now()
+  this.frames = 0
+    
+  var currentTime = time
+  var passedTime = currentTime - this.lastTime
+  
+  this.delta = passedTime * 0.06
+  
+  if (this.delta > 2.3) {
+    this.delta = 2.3
   }
+  
+  this.lastTime = currentTime
 
-  time.prototype.update = function() {
-    this.frames++
-    var time = Date.now()
-    this.frames = 0
-      
-    var currentTime = time
-    var passedTime = currentTime - this.lastTime
-    
-    this.delta = passedTime * 0.06
-    
-    if (this.delta > 2.3) {
-      this.delta = 2.3
-    }
-    
-    this.lastTime = currentTime
+  return this.delta
+};
 
-    return this.delta
-  }
-
-  return time
-})()
+module.exports = Time;
