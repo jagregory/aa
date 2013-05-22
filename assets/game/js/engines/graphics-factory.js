29b430e062c17f27d6621c2981dbd81703fa9c84
Romain Prieto <romain.prieto@gmail.com>
2013-05-23 01:48:17 +1000

    fixed graphics factory pixels sizes

--- assets/game/js/engines/graphics-factory.js
+++ assets/game/js/engines/graphics-factory.js
@@ -1,8 +1,14 @@
+var world = require('../world');
 
 exports.sprite = function(image, width, height) {
   var texture = PIXI.Texture.fromImage(image);
   var sprite = new PIXI.Sprite(texture);
-  sprite.anchor.x = width / 2;
-  sprite.anchor.y = height / 2;
+  sprite.width = world.toPixels(width);  
+  sprite.height = world.toPixels(height);
+  sprite.anchor.x = sprite.width  / 2;
+  sprite.anchor.y = sprite.height / 2;
+  console.log('anchor = ', sprite.anchor)
+  sprite.position.x = 0;
+  sprite.position.y = 0;
   return sprite;
 };
