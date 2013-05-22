84e7340bad5c5a7c9923e1b9ff6fc9cdffe008e3
Romain Prieto <romain.prieto@gmail.com>
2013-05-23 02:29:30 +1000

    Kick-off has 3, 2, 1 countdown (console.log -> needs to be on screen text)

--- assets/game/js/states/kickoff.js
+++ assets/game/js/states/kickoff.js
@@ -1,22 +1,14 @@
 // add the ball
 // countdown 3...2...1...
 
+var _ = require('../../../3rdparty/underscore-min');
 var Ball = require('../entities/ball');
 
 function KickOff(game) {
   
   this.enter = function() {
-    
-    var ball = new Ball('ball', game.physics);
-    game.addEntity(ball);
-    
-    // Display countdown 3....2...1....
-    // Then kick the ball and transition
-    setTimeout(function() {
-      ball.body.SetAwake(true);
-      ball.body.SetLinearVelocity(new Box2D.Common.Math.b2Vec2(16, 16));
-      game.transition('go');
-    }, 500);
+    game.addEntity(new Ball('ball', game.physics));
+    countdown(3);
   };
   
   this.exit = function() {
@@ -27,6 +19,22 @@ function KickOff(game) {
   
   this.on = function(message, args) {
   };
+  
+  function countdown(val) {
+    if (val == 0) {
+      go();
+    } else {
+      console.log(val);
+      setTimeout(_.partial(countdown, --val), 1000);
+    }
+  }
+  
+  function go() {
+    var ball = game.getEntity('ball');
+    ball.body.SetAwake(true);
+    ball.body.SetLinearVelocity(new Box2D.Common.Math.b2Vec2(16, 16));
+    game.transition('go');
+  }
   
 }
 
