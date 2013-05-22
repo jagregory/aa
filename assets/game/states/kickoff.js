2ab77afed49b4707742aa38e89098cc8b11ebb23
Romain Prieto <romain.prieto@gmail.com>
2013-05-22 19:19:48 +1000

    KickOff state kick the ball after the countdown

--- assets/game/states/kickoff.js
+++ assets/game/states/kickoff.js
@@ -9,14 +9,13 @@ function KickOff(game) {
     var ball = new Ball(game, game.physics);
     game.track(ball);
     
-    ball.body.SetAwake(true);
-    ball.body.SetLinearVelocity(new Box2D.Common.Math.b2Vec2(10, 10));
-    
     // Display countdown 3....2...1....
     // Then kick the ball and transition
     setTimeout(function() {
+      ball.body.SetAwake(true);
+      ball.body.SetLinearVelocity(new Box2D.Common.Math.b2Vec2(10, 10));
       game.transition('go');
-    }, 1000);
+    }, 500);
   };
   
   this.exit = function() {
