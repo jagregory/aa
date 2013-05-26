b9240fec5dc4cca91683229b92393f55f4e6cfd2
James Gregory <james@jagregory.com>
2013-05-26 16:50:42 +1000

    Dumb particle pool to prevent pause when creating a lot of particles at once.

--- assets/game/js/entities/explosion.js
+++ assets/game/js/entities/explosion.js
@@ -1,4 +1,5 @@
-var Entity = require('../entity'),
+var _ = require('../../../3rdparty/underscore-min'),
+  Entity = require('../entity'),
   World = require('../world')
 
 var randomSign = function() {
@@ -23,12 +24,21 @@ Partical.prototype = Object.create(PIXI.Sprite.prototype)
 
 var resetParticle = function(particle) {
   particle.alpha = 1
-  particle.speed.x = Math.random() * randomSign()
-  particle.speed.y = Math.random() * randomSign()
+  particle.speed.x = (0.5 + Math.random()) * randomSign()
+  particle.speed.y = (0.5 + Math.random()) * randomSign()
   particle.acceleration.x = (0.1 + Math.random()) * randomSign()
   particle.acceleration.y = (0.1 + Math.random()) * randomSign()
   particle.position.x = 0
   particle.position.y = 0
+  particle.visible = true
+}
+
+var particlePool = []
+for (var i = 0; i <= 10000; i++) {
+  var particle = new Partical()
+  particle.blendMode = PIXI.blendModes.SCREEN
+  particlePool.push(particle)
+  resetParticle(particle)
 }
 
 var Explosion = function(origin) {
@@ -36,10 +46,10 @@ var Explosion = function(origin) {
   this.sprite.position.x = World.toPixels(origin.x)
   this.sprite.position.y = World.toPixels(origin.y)
   this.particles = []
+  this.ttl = 0
 
   for (var i = 0; i <= 1000; i++) {
-    var particle = new Partical()
-    particle.blendMode = PIXI.blendModes.SCREEN
+    var particle = particlePool[i]
     this.sprite.addChild(particle)
     this.particles.push(particle)
     resetParticle(particle)
@@ -48,8 +58,14 @@ var Explosion = function(origin) {
 
 Explosion.prototype = new Entity()
 
-Explosion.prototype.update = function() {
+Explosion.prototype.update = function(delta) {
+  this.ttl -= delta
+
   this.particles.forEach(function(particle) {
+    if (!particle) {
+      return
+    }
+
     particle.position.x += (0.5 * particle.speed.x)
     particle.position.y += (0.5 * particle.speed.y)
     particle.speed.x += 0.05 * particle.acceleration.x
@@ -59,10 +75,21 @@ Explosion.prototype.update = function() {
       particle.alpha *= 0.92
     }
 
-    if (particle.alpha <= 0.0001) {
-      resetParticle(particle)
+    if (particle.alpha <= (Math.random() * 5) / 50) {
+      if (this.ttl > 0) {
+        // only reuse particle if we're still alive
+        resetParticle(particle)
+      } else {
+        particle.visible = false
+      }
     }
   }.bind(this))
+
+  if (this.ttl <= 0 && !_.findWhere(this.particles, { visible: true })) {
+    // TODO: I don't know how this is supposed to work. Destroy expects
+    // the game and physics engines to be passed to it.
+    // this.destroy()
+  }
 }
 
 module.exports = Explosion
