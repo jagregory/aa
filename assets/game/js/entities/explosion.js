6b130adae95ab95ab3de2173630b3d32ebd0c9bf
James Gregory <james@jagregory.com>
2013-05-26 15:48:36 +1000

    Particles explode out

--- assets/game/js/entities/explosion.js
+++ assets/game/js/entities/explosion.js
@@ -1,5 +1,13 @@
 var Entity = require('../entity'),
-  World = require('../world');
+  World = require('../world')
+
+var randomSign = function() {
+  return Math.random() < 0.5 ? -1 : 1
+}
+
+var distance = function(a, b) {
+  return Math.sqrt((b.x - a.x) * (b.x - a.x) + (b.y - a.y) * (b.y - a.y))
+}
 
 var Partical = function() {
   PIXI.Sprite.call(this, PIXI.Texture.fromImage('/game/images/particle.png'))
@@ -14,6 +22,15 @@ var Partical = function() {
 Partical.constructor = Partical
 Partical.prototype = Object.create(PIXI.Sprite.prototype)
 
+var resetParticle = function(particle) {
+  particle.alpha = 1
+  particle.speed.x = Math.random() * randomSign()
+  particle.speed.y = Math.random() * randomSign()
+  particle.accel = 0.1 + Math.random()
+  particle.position.x = 0
+  particle.position.y = 0
+}
+
 var Explosion = function(origin) {
   this.sprite = new PIXI.DisplayObjectContainer()
   this.sprite.position.x = World.toPixels(origin.x)
@@ -25,12 +42,7 @@ var Explosion = function(origin) {
     particle.blendMode = "w"
     this.sprite.addChild(particle)
     this.particles.push(particle)
-    particle.count = 0
-    particle.speed.x = 1 * (Math.random() - 0.5)
-    particle.speed.y = 2 + 3 * Math.random()
-    particle.accel = 0.1 + Math.random()
-    particle.position.x = 0
-    particle.position.y = 0
+    resetParticle(particle)
   }
 }
 
@@ -38,21 +50,19 @@ Explosion.prototype = new Entity()
 
 Explosion.prototype.update = function() {
   this.particles.forEach(function(particle) {
-    particle.position.x += 0.5 * particle.speed.x
-    particle.position.y -= particle.speed.y
+    particle.position.x += (0.5 * particle.speed.x)
+    particle.position.y += (0.5 * particle.speed.y)
+    particle.speed.x += 0.0050 * particle.accel
     particle.speed.y += 0.0050 * particle.accel
 
-    if (-200 > particle.position.y) {
+    if (distance({ x: 0, y: 0 }, particle.position) >= 100) {
       particle.alpha *= 0.92
     }
 
-    if (-300 > particle.position.y) {
-      particle.position.y = Math.random() - 0.5
-      particle.position.x = 0
-      particle.alpha = 1
-      particle.speed.y = 1
+    if (particle.alpha <= 0.0001) {
+      resetParticle(particle)
     }
-  })
+  }.bind(this))
 }
 
 module.exports = Explosion
