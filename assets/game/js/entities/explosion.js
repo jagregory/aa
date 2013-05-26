c080c63e20b9ba02c398b7234a08ffdcac75c681
James Gregory <james@jagregory.com>
2013-05-26 16:08:13 +1000

    More particles

--- assets/game/js/entities/explosion.js
+++ assets/game/js/entities/explosion.js
@@ -10,12 +10,11 @@ var distance = function(a, b) {
 }
 
 var Partical = function() {
-  PIXI.Sprite.call(this, PIXI.Texture.fromImage('/game/images/particle.png'))
+  PIXI.Sprite.call(this, PIXI.Texture.fromImage('/game/images/particle-orange.png'))
   this.anchor.x = 0.5
   this.anchor.y = 0.5
-  this.count = 0
   this.speed = new PIXI.Point
-  this.accel = 0
+  this.acceleration = new PIXI.Point
   this.width = 32
   this.height = 32
 }
@@ -26,7 +25,8 @@ var resetParticle = function(particle) {
   particle.alpha = 1
   particle.speed.x = Math.random() * randomSign()
   particle.speed.y = Math.random() * randomSign()
-  particle.accel = 0.1 + Math.random()
+  particle.acceleration.x = (0.1 + Math.random()) * randomSign()
+  particle.acceleration.y = (0.1 + Math.random()) * randomSign()
   particle.position.x = 0
   particle.position.y = 0
 }
@@ -37,9 +37,9 @@ var Explosion = function(origin) {
   this.sprite.position.y = World.toPixels(origin.y)
   this.particles = []
 
-  for (var i = 0; i <= 100; i++) {
+  for (var i = 0; i <= 1000; i++) {
     var particle = new Partical()
-    particle.blendMode = "w"
+    particle.blendMode = PIXI.blendModes.SCREEN
     this.sprite.addChild(particle)
     this.particles.push(particle)
     resetParticle(particle)
@@ -52,8 +52,8 @@ Explosion.prototype.update = function() {
   this.particles.forEach(function(particle) {
     particle.position.x += (0.5 * particle.speed.x)
     particle.position.y += (0.5 * particle.speed.y)
-    particle.speed.x += 0.0050 * particle.accel
-    particle.speed.y += 0.0050 * particle.accel
+    particle.speed.x += 0.05 * particle.acceleration.x
+    particle.speed.y += 0.05 * particle.acceleration.y
 
     if (distance({ x: 0, y: 0 }, particle.position) >= 100) {
       particle.alpha *= 0.92
