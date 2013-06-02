5fba3a913442078df6f5e9daaf392b65dbf43a8a
Romain Prieto <romain.prieto@gmail.com>
2013-06-02 16:20:51 +1000

    Default admin page = register new player

--- assets/admin/js/admin.js
+++ assets/admin/js/admin.js
@@ -3,10 +3,10 @@ var routie = require('../../3rdparty/routie');
 window.Admin = function() {
   
   routie({
-      '':            require('./controllers/game'),
-      '/game':       require('./controllers/game'),
-      '/players':    require('./controllers/players'),
+      '':            require('./controllers/register'),
       '/register':   require('./controllers/register'),
+      '/players':    require('./controllers/players'),
+      '/game':       require('./controllers/game'),
   });
   
 };
