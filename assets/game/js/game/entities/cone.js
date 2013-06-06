d08e14bd856ee0bec9ba5d7420bc4cc211718c85
Romain Prieto <romain.prieto@gmail.com>
2013-06-07 00:15:43 +1000

    Stadium definition + cone art

---
+++ assets/game/js/game/entities/cone.js
@@ -0,0 +1,35 @@
+var PF          = require('../../engine/physics-factory');
+var GF          = require('../../engine/graphics-factory');
+var Entity      = require('../../engine/entity');
+var world       = require('../../engine/world');
+
+function Cone(id, x, y) {
+  this.id = id;
+  this.bodySpec = {
+    body: PF.dynamic({
+      x: x,
+      y: y
+    }),
+    fixture: PF.fixture({
+      shape:      PF.shape.box(2, 2.5),
+      dynamics:   {density: 1, friction: 0, restitution: 1},
+      category:   PF.categories.ARENA,
+      collision:  PF.categories.ALL
+    })
+  };
+  this.sprite = GF.sprite('/game/images/cone.png', 2, 4);
+  this.sprite.position.x = world.toPixels(x);
+  this.sprite.position.y = world.toPixels(y);
+}
+
+Cone.prototype = new Entity();
+
+// Cone.prototype.update = function(delta) {
+//   Entity.prototype.update.call(this, delta);
+//   // We should be able to specify "0.5", and not have to update it constantly
+//   // Need to check our changes to PIXI
+//   this.sprite.anchor.x = this.sprite.texture.width  / 2;
+//   this.sprite.anchor.y = this.sprite.texture.height / 3;
+// };
+
+module.exports = Cone;
---
+++ assets/game/js/game/entities/stadium.js
@@ -0,0 +1,37 @@
+var CompoundEntity  = require('../../engine/compound-entity');
+var Background      = require('./background');
+var Wall            = require('./wall');
+var Cone            = require('./cone');
+var Goal            = require('./goal');
+var world           = require('../world');
+
+var PI     = 3.14;
+var width  = world.width;
+var height = world.height;
+var top    = 3;
+var left   = 0.5;
+var right  = world.width  - 0.5;
+var bottom = world.height - 1.5;
+
+function Stadium() {
+  
+  this.entities.push(new Background());
+  
+  this.entities.push(new Wall('wall-top',    width / 2, top,            width, 1           ));
+  this.entities.push(new Wall('wall-bottom', width / 2, bottom,         width, 1           ));
+  this.entities.push(new Wall('wall-left1',  left  + 2, height * 1.0/6, 1,     height / 2.5));
+  this.entities.push(new Wall('wall-left2',  left  + 2, height * 4.9/6, 1,     height / 2.5));
+  this.entities.push(new Wall('wall-right1', right - 2, height * 1.0/6, 1,     height / 2.5));
+  this.entities.push(new Wall('wall-right2', right - 2, height * 4.9/6, 1,     height / 2.5));
+  
+  this.entities.push(new Cone('cone1', width / 12 * 5, height / 3 * 1));
+  this.entities.push(new Cone('cone2', width / 12 * 7, height / 3 * 2));
+  
+  this.entities.push(new Goal('goalp1', 0, left,  height / 2, 2, 8));
+  this.entities.push(new Goal('goalp2', 1, right, height / 2, 2, 8));
+  
+}
+
+Stadium.prototype = new CompoundEntity();
+
+module.exports = Stadium;
