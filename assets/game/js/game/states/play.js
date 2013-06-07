fc9a6db2363939ed452e0b657704792b99417c7a
Romain Prieto <romain.prieto@gmail.com>
2013-06-07 21:14:23 +1000

    Game remaining time cannot be negative

--- assets/game/js/game/states/play.js
+++ assets/game/js/game/states/play.js
@@ -8,11 +8,12 @@ function Play(engine, game) {
   };
   
   this.update = function(delta) {
-    game.timeRemaining  -= delta;
-    // var seconds = Math.floor(countdownMillis / 1000);
-    // engine.getEntity('hud').updateTime(seconds);
-    if (game.timeRemaining <= 0) {
-      engine.transition('end');
+    game.timeRemaining -= delta;
+    if (game.timeRemaining < 0) {
+      game.timeRemaining = 0;
+    }
+    if (game.timeRemaining === 0) {
+      game.transition('end');
     }
   };
   
