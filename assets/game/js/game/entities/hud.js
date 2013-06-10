9218b9a240f3a90f939354857c2a1e10ee06a36f
James Gregory <james@jagregory.com>
2013-06-10 12:11:59 +1000

    Split scores into separate sprite and position them over the score holes on the HUD

--- assets/game/js/game/entities/hud.js
+++ assets/game/js/game/entities/hud.js
@@ -12,12 +12,20 @@ function Hud(text) {
   this.id = 'hud';
   
   this.p1Name = GF.text('John Doe', 25);
-  this.p1Name.position.x = MARGIN + HUD_TEXT_X;
+  this.p1Name.position.x = MARGIN + HUD_TEXT_X + 10;
   this.p1Name.position.y = MARGIN + 10;
+
+  this.p1Score = GF.text('0', 35, {fill: '#fff', stroke: '#000'});
+  this.p1Score.position.x = (userInterface.width / 2) - 265;
+  this.p1Score.position.y = MARGIN + 3;
 
   this.p2Name = GF.text('John Doe', 25);
   this.p2Name.position.x = userInterface.width - MARGIN - HUD_WIDTH + HUD_TEXT_X;
   this.p2Name.position.y = MARGIN + 10;
+
+  this.p2Score = GF.text('0', 35, {fill: '#fff', stroke: '#000'});
+  this.p2Score.position.x = (userInterface.width / 2) + 200;
+  this.p2Score.position.y = MARGIN + 3;
 
   this.time = GF.text(fourDigits(0), 35, {fill: '#fff', stroke: '#000'});
   this.time.position.x = userInterface.width / 2 - this.time.width / 2;
@@ -29,21 +37,27 @@ Hud.prototype = new Entity();
 
 Hud.prototype.create = function(engine, game) {
   engine.graphics.add(this.p1Name);
+  engine.graphics.add(this.p1Score);
   engine.graphics.add(this.p2Name);
+  engine.graphics.add(this.p2Score);
   engine.graphics.add(this.time);
 };
 
 Hud.prototype.destroy = function(engine, game) {
   engine.graphics.remove(this.p1Name);
+  engine.graphics.remove(this.p1Score);
   engine.graphics.remove(this.p2Name);
+  engine.graphics.remove(this.p2Score);
   engine.graphics.remove(this.time);
 };
 
 Hud.prototype.update = function(engine, game, delta) {
   var p1 = game.players[0];
   var p2 = game.players[1];
-  this.p1Name.setText(p1.name + ' ' + p1.score);
-  this.p2Name.setText(p2.name + ' ' + p2.score);
+  this.p1Name.setText(p1.name);
+  this.p1Score.setText(p1.score.toString());
+  this.p2Name.setText(p2.name);
+  this.p2Score.setText(p2.score.toString());
   this.time.setText(fourDigits(game.timeRemaining));
 };
 
