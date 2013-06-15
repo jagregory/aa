7e1869c50c01fd0c13673b365ba9dc40fd23195e
Romain Prieto <romain.prieto@gmail.com>
2013-06-15 16:50:29 +1000

    Fixed the world to be 16/10 (full screen macbook pro)

--- assets/game/js/game/entities/stadium.js
+++ assets/game/js/game/entities/stadium.js
@@ -8,10 +8,10 @@ var world           = require('../world');
 var PI     = 3.14;
 var width  = world.width;
 var height = world.height;
-var top    = 3;
+var top    = 3.8;
 var left   = 0.5;
 var right  = world.width  - 0.5;
-var bottom = world.height - 1.5;
+var bottom = world.height - 2.7;
 
 function Stadium() {
   
@@ -20,22 +20,22 @@ function Stadium() {
   this.entities = [];
   this.entities.push(new Background());
   
-  this.entities.push(new Wall('wall-top',    width / 2, top,            width, 1,            0));
-  this.entities.push(new Wall('wall-bottom', width / 2, bottom,         width, 1,            0));
-  this.entities.push(new Wall('wall-left1',  left  + 2.8, height * 1.0/6, 1,     height / 2.5, 0.15));
-  this.entities.push(new Wall('wall-left2',  left  + 1.1, height * 4.9/6, 1,     height / 2.5,   0.05));
-  this.entities.push(new Wall('wall-right1', right - 2.5, height * 1.0/6, 1,     height / 2.5, -0.10));
-  this.entities.push(new Wall('wall-right2', right - 1.2, height * 4.9/6, 1,     height / 2.5, -0.05));
-  this.entities.push(new Wall('wall-goal-left-top',     0, height / 2 - 4.5, 4,   1, 0));
-  this.entities.push(new Wall('wall-goal-left-bottom',  0, height / 2 + 3.5, 2.5, 1, 0));
-  this.entities.push(new Wall('wall-goal-right-top',    width, height / 2 - 4.5, 4,   1, 0));
-  this.entities.push(new Wall('wall-goal-right-bottom', width, height / 2 + 3.5, 2.5, 1, 0));
+  this.entities.push(new Wall('wall-top',               width / 2,      top,                width,   1,               0));
+  this.entities.push(new Wall('wall-bottom',            width / 2,      bottom,             width,   1,               0));
+  this.entities.push(new Wall('wall-left1',             left  + 2.8,    height * 1.0/6,     1,       height / 2.5,    0.15));
+  this.entities.push(new Wall('wall-left2',             left  + 1.1,    height * 4.9/6,     1,       height / 2.5,    0.05));
+  this.entities.push(new Wall('wall-right1',            right - 2.5,    height * 1.0/6,     1,       height / 2.5,   -0.10));
+  this.entities.push(new Wall('wall-right2',            right - 1.2,    height * 4.9/6,     1,       height / 2.5,   -0.05));
+  this.entities.push(new Wall('wall-goal-left-top',     0,              height / 2 - 5.4,   4,       1,               0));
+  this.entities.push(new Wall('wall-goal-left-bottom',  0,              height / 2 + 4.2,   2.7,     1,               0));
+  this.entities.push(new Wall('wall-goal-right-top',    width,          height / 2 - 5.4,   4,       1,               0));
+  this.entities.push(new Wall('wall-goal-right-bottom', width,          height / 2 + 4.1,   2.5,     1,               0));
     
   this.entities.push(new Cone('cone1', width / 12 * 5, height / 3 * 1));
   this.entities.push(new Cone('cone2', width / 12 * 7, height / 3 * 2));
   
-  this.entities.push(new Goal('goalp1', 0, 0, height / 2, 0.5, 8));
-  this.entities.push(new Goal('goalp2', 1, world.width, height / 2, 0.5, 8));
+  this.entities.push(new Goal('goalp1', 0,  0,            height / 2, 0.5, 10));
+  this.entities.push(new Goal('goalp2', 1,  world.width,  height / 2, 0.5, 10));
   
 }
 
--- assets/game/js/game/world.js
+++ assets/game/js/game/world.js
@@ -1,3 +1,4 @@
 
+// Most screens will have a ratio of 16/9 or 16/10
 exports.width  = 60;
-exports.height = 30;
+exports.height = 60 / (16/10);
