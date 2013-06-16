a0e42727aeb38a930b73a852f4ad058621a4c36e
James Gregory <james@jagregory.com>
2013-06-16 14:27:01 +1000

    Snazzed up leaderboard

--- assets/game/js/intro/entities/leaderboard.js
+++ assets/game/js/intro/entities/leaderboard.js
@@ -2,8 +2,29 @@ var Entity = require('../../engine/entity')
 var GF  = require('../../engine/graphics-factory');
 var userInterface = require('../../engine/user-interface');
 
+var formatAsRank = function(num) {
+  if (num === 1) {
+    return num + 'st'
+  } else if (num === 2) {
+    return num + 'nd'
+  } else if (num === 3) {
+    return num + 'rd'
+  } else {
+    return num + 'th'
+  }
+}
+
+var unit = function(n) {
+  return (userInterface.width / 100) * n
+}
+
 function Leaderboard(id) {
-  
+  var DefaultTextOptions = {
+    strokeThickness: unit(0.7),
+    fill: '#01518d'
+  }
+  var DefaultFontSize = unit(5)
+
   this.id = id;
   this.players = []
   
@@ -13,7 +34,7 @@ function Leaderboard(id) {
     success: function(data) {
       this.players = data.sort(function(x,y) {
         return y.topScore - x.topScore
-      }).slice(0, 10)
+      }).slice(0, 5)
     }.bind(this)
   })
 
@@ -21,26 +42,32 @@ function Leaderboard(id) {
     GF.uiSprite('/game/images/leaderboard.png', userInterface.width, userInterface.height)
   ];
 
-  var y = 0
-
-  var titleSprite = GF.text('LEADERBOARD', 65, {
-    strokeThickness: 4
-  })
-  titleSprite.position.x = userInterface.width / 2 - titleSprite.width  / 2;
-  titleSprite.position.y = (y += 50);
-
-  y += titleSprite.height
-
-  this.sprites.push(titleSprite)
+  var currentY = unit(21)
+  var i = 1
 
   this.players.forEach(function(player) {
-    var textSprite = GF.text(player.firstName + ' ... ' + player.topScore, 30, {
-      strokeThickness: 4
-    })
-    textSprite.position.x = userInterface.width / 2 - titleSprite.width  / 2;
-    textSprite.position.y = (y += 10)
-    y += textSprite.height
-    this.sprites.push(textSprite)
+    var rankSprite = GF.text(formatAsRank(i), DefaultFontSize, DefaultTextOptions)
+    rankSprite.position.y = currentY
+    rankSprite.position.x = unit(5)
+    this.sprites.push(rankSprite)
+
+    var playerNameSprite = GF.text((player.firstName + ' ' + player.lastName.substring(0, 1)).toUpperCase(), DefaultFontSize, $.extend({}, DefaultTextOptions, { fill: '#bf0000' }))
+    playerNameSprite.position.x = unit(18)
+    playerNameSprite.position.y = currentY
+    this.sprites.push(playerNameSprite)
+
+    var companySprite = GF.text((player.company || '?').toUpperCase(), unit(3), $.extend({}, DefaultTextOptions, { strokeThickness: unit(0.5) }))
+    companySprite.position.x = playerNameSprite.position.x + playerNameSprite.width + unit(2)
+    companySprite.position.y = currentY + unit(1.5)
+    this.sprites.push(companySprite)
+
+    var scoreSprite = GF.text(player.topScore + ' GOALS', DefaultFontSize, DefaultTextOptions)
+    scoreSprite.position.x = userInterface.width - scoreSprite.width - unit(5)
+    scoreSprite.position.y = currentY
+    this.sprites.push(scoreSprite)
+    
+    currentY += playerNameSprite.height + unit(0.6)
+    i += 1
   }.bind(this))
 }
 
